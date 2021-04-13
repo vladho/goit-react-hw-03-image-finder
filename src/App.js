2ff1846663146221801c6b52d664d30468692b53
Vlad <vladhopilo@gmail.com>
2021-04-13 14:40:25 +0300

    up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css"
-import { Component } from "react"
-import ImageGallery from "./Components/ImageGallery/ImageGallery"
-import Searchbar from "./Components/Searchbar/Searchbar"
-import servicesApi from "./services/gallery-api"
-import Button from "./Components/Button/Button"
-import Loader from "react-loader-spinner"
+import "./App.css";
+import { Component } from "react";
+import ImageGallery from "./Components/ImageGallery/ImageGallery";
+import Searchbar from "./Components/Searchbar/Searchbar";
+import servicesApi from "./services/gallery-api";
+import Button from "./Components/Button/Button";
+import Loader from "react-loader-spinner";
 
 class App extends Component {
   state = {
@@ -14,11 +14,11 @@ class App extends Component {
     currentPage: 1,
     error: null,
     showModal: false,
-  }
+  };
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchQuery !== this.state.searchQuery) {
-      this.fetchImage()
+      this.fetchImage();
     }
   }
 
@@ -27,48 +27,55 @@ class App extends Component {
       image: [],
       searchQuery: query,
       currentPage: 1,
-    })
-  }
+    });
+  };
 
   fetchImage = () => {
-    const { searchQuery, currentPage } = this.state
-    const data = { searchQuery, currentPage }
-    this.setState({ isLoading: true })
+    const { searchQuery, currentPage } = this.state;
+    const data = { searchQuery, currentPage };
+    this.setState({ isLoading: true });
 
     servicesApi
       .fetchImage(data)
       .then((data) =>
         this.setState((prevState) => ({
-          image: [...prevState.image, ...data.data.hits.map((el) => ({ ...el }))],
+          image: [
+            ...prevState.image,
+            ...data.data.hits.map((el) => ({ ...el })),
+          ],
           currentPage: prevState.currentPage + 1,
         }))
       )
       .then(() => this.scroll())
       .catch((error) => this.setState({ error }))
-      .finally(() => this.setState({ isLoading: false }))
-  }
+      .finally(() => this.setState({ isLoading: false }));
+  };
 
   scroll = () => {
     window.scrollTo({
       top: document.documentElement.scrollHeight,
       behavior: "smooth",
-    })
-  }
+    });
+  };
   render() {
-    const isLoading = this.state.isLoading
-    const ifSearchNull = this.state.image.length > 0 && !isLoading
-    const error = this.state.error
+    const isLoading = this.state.isLoading;
+    const ifSearchNull = this.state.image.length > 0 && !isLoading;
+    const error = this.state.error;
 
     return (
       <div className="App">
         <Searchbar onSubmit={this.onChangeQuery} />
         {error && <h1>Ooops what's wrong</h1>}
         <ImageGallery imageURL={this.state.image} />
-        {isLoading && <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />}
-        {ifSearchNull && <Button look={this.state.searchQuery} onClick={this.fetchImage} />}
+        {isLoading && (
+          <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />
+        )}
+        {ifSearchNull && (
+          <Button look={this.state.searchQuery} onClick={this.fetchImage} />
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
